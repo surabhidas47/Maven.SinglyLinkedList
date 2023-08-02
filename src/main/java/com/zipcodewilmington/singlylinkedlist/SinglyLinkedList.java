29f180d75f319891fc8611e08631409c06d9cd22
surabhi <surabhi.das@outlook.com>
2023-08-02 10:30:15 -0400

    added method stubs

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,54 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.util.Comparator;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList <T> implements LinkedListIface{
+
+    public SinglyLinkedList(){
+
+    }
+
+
+    @Override
+    public void add(Object data) {
+
+    }
+
+    @Override
+    public void remove(int index) throws IndexOutOfBoundsException {
+
+    }
+
+    @Override
+    public Object get(int index) throws IndexOutOfBoundsException {
+        return null;
+    }
+
+    @Override
+    public boolean contains(Object data) {
+        return false;
+    }
+
+    @Override
+    public int find(Object data) {
+        return 0;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public SinglyLinkedList copy() {
+        return null;
+    }
+
+    @Override
+    public void sort(Comparator comparator) {
+
+    }
 }
